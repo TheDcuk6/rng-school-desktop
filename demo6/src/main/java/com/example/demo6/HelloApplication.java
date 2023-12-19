0f52a49445b07dcd0fdc938ec32b7f067edd65f1
TheDuck6 <115145927+TheDcuk6@users.noreply.github.com>
2023-12-19 09:26:29 +0100

    Add files via upload

---
+++ demo6/src/main/java/com/example/demo6/HelloApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo6;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 550);
+        stage.getIcons().add(new Image(HelloApplication.class.getResourceAsStream("wikipedia_PNG10.png")));
+        stage.setTitle("Generator Liczb Losowych!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
